0f2b34c1d6d75a5c8632e7d56a68c9df884072f0
LAKE <lake.pnt@gmail.com>
2025-10-01 14:13:53 +0900

    SECURITY FIX

--- src/core/SECURITY.py
+++ src/core/SECURITY.py
@@ -14,7 +14,7 @@ async def Restrict(ctx: ApplicationContext):
     if ctx.guild is None:
         await ctx.respond("❌ このコマンドはサーバー内でのみ使用できます。", ephemeral=True)
         return False
-    if not ctx.author.guild_permissions.manage_guild and ctx.author.id != DEV and ctx.author != POINT_ADMIN:
+    if not ctx.author.guild_permissions.manage_guild and ctx.author.id != DEV and ctx.author.id != POINT_ADMIN:
         await ctx.respond("❌ このコマンドを使うにはサーバー管理権限が必要です。", ephemeral=True)
         CONSOLE.text(f"{ctx.author.name}'s Access was denied!", CONSOLE.WARN)
         return False
